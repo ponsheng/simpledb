67a128cc1d552fab14cee422a2b4a5db0e40ca53
ponsheng <a7061120@gmail.com>
2019-06-22 01:41:24 +0800

    Add start, vmmap cmd

--- src/simpledb.c
+++ src/simpledb.c
@@ -12,7 +12,7 @@ enum state {
     DB_INIT, DB_LOADED, DB_RUNNING};
 enum state dbstate = DB_INIT;
 char *elf = NULL;
-
+elf_shdr_t text_section;
 void print_elf(elf_handle_t *eh) {
     elf_strtab_t *tab = NULL;
     if (!eh) {
@@ -29,17 +29,21 @@ void print_elf(elf_handle_t *eh) {
             break;
         }
     }
-    printf("program '%s' loaded. entry point 0x4000b0 0x%lx, vaddr 0x%llx 0x4000b0, offset 0x%llx 0xb0, size 0x%llx 0x23\n", elf, eh->entrypoint, eh->shdr[i].addr, eh->shdr[i].offset, eh->shdr[i].size);
-    elf_close(eh); 
+    text_section = eh->shdr[i];
+    printf("program '%s' loaded. entry point 0x4000b0 0x%lx, vaddr 0x%llx 0x4000b0, offset 0x%llx 0xb0, size 0x%llx 0x23\n", elf, eh->entrypoint, text_section.addr,  text_section.offset, text_section.size);
+    //elf_close(eh); 
 }
+#define N 200
 int main(int argc, char**argv) {
     char *pos;
     int size = 0;
     elf_handle_t *eh = NULL;
     elf_init();
-    pid_t pid;
+    pid_t pid, ppid;
     char cmd[100];
     char *tok = NULL;
+    char buf[N];
+    ppid = getpid();
     if (argc > 1) {
         elf = argv[1];
         eh = elf_open(elf);
@@ -77,19 +81,49 @@ int main(int argc, char**argv) {
         }
         if (strncmp(cmd, "run", 3)==0 || strncmp(cmd,"r",1) ==0) {
             ptrace(PTRACE_CONT,pid,NULL,NULL);
-        } else if (strncmp(cmd, "load", 4) ==0) {
+            wait(NULL);
+            continue;
+        } else if (strncmp(cmd, "start", 5) ==0) {
+            if (dbstate != DB_LOADED) {
+                goto INVALID;
+            }
+            pid = fork();
+            if (pid) {
+                dbstate = DB_RUNNING;
 
+            } else {
+                ptrace(PTRACE_TRACEME,0,NULL,NULL);
+                execve(elf,argv, environ);
+            }
+            continue;
+        } else if (strncmp(cmd, "vmmap", 5) ==0 || cmd[0] == 'm') {
+            if (dbstate == DB_LOADED) {
+
+            } else if (dbstate == DB_RUNNING) {
+                char file_name[100];
+                sprintf(file_name, "/proc/%d/maps", pid);
+                FILE *f = fopen(file_name, "r");
+                if (!f) {
+                    printf("Open file %s failed\n", file_name);
+                    continue;
+                }
+                fgets(buf, N,f);
+                while(fgets(buf, N, f) != NULL) {
+                    printf("%s", buf);
+                }
+                fclose(f);
+                continue;
+            }
         } else if (strncmp(cmd, "exit", 4)==0) {
             break;
         } else {
-INVALID:
-            tok = strtok(cmd, " ");
-            if (tok != NULL) {
-                printf("'%s' is invalid command\n", cmd);
-            }
-            continue; 
+        //    continue; 
         }
-        //wait(NULL);
+INVALID:
+        tok = strtok(cmd, " ");
+        if (tok != NULL) {
+            printf("'%s' is invalid command\n", cmd);
+        }
     }
 /*        int ret = kill(pid,9);
         if (ret) {
@@ -98,8 +132,6 @@ INVALID:
         }
         wait(NULL);
      } else {
-        ptrace(PTRACE_TRACEME,0,NULL,NULL);
-        execve(exe,argv, environ);
      }*/
     return 0;
 }
