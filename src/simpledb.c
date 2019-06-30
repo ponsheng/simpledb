1346b4564aa4f9fd8e674e2a2eec8edeb846cf3f
ponsheng <a7061120@gmail.com>
2019-06-30 22:31:39 +0800

    Add cmd help, break, list

--- src/simpledb.c
+++ src/simpledb.c
@@ -8,32 +8,18 @@
 #include <string.h>
 #include <elftool.h>
 
+enum state { DB_INIT = 1, DB_LOADED=2, DB_RUNNING=4};
+
+struct break_point {
+    void *addr;
+    int used;
+};
+
+struct break_point bps[10];
+int bpcount=0;
+
 extern char **environ;
-enum state {
-    DB_INIT = 1, DB_LOADED=2, DB_RUNNING=4};
-
 enum state dbstate = DB_INIT;
-
-void print_elf(char *elf, elf_handle_t *eh) {
-    elf_strtab_t *tab = NULL;
-    if (!eh) {
-        puts("Failed");
-        return;
-    }
-    elf_load_all(eh);
-    for (tab = eh->strtab; tab != NULL; tab = tab->next) {
-        if (tab->id == eh->shstrndx) break;
-    }
-    int i;
-    for (i = 0; i < eh->shnum; i++) {
-        if (strncmp(&tab->data[eh->shdr[i].name], ".text", 5) == 0) {
-            break;
-        }
-    }
-    elf_shdr_t text_section = eh->shdr[i];
-    printf("program '%s' loaded. Entry point 0x%lx. [TEXT] vaddr:0x%llx, offset:0x%llx, size:0x%llx\n", elf, eh->entrypoint, text_section.addr,  text_section.offset, text_section.size);
-    elf_close(eh); 
-}
 
 int checkS(enum state s) {
     if (s & dbstate) {
@@ -45,6 +31,8 @@ int checkS(enum state s) {
 int parse_input(char *cmd, char **arg, int *arg_count);
 elf_handle_t *open_elf(char *elf_name);
 void print_eip(pid_t pid);
+void print_elf(char *elf, elf_handle_t *eh);
+void print_help();
 
 #define N 200
 #define COMMAND_BUFFER_SIZE 100
@@ -115,18 +103,20 @@ int main(int argc, char**argv) {
                 }
             }
             printf("run %d instructions\n", count);
+            dbstate = DB_LOADED;
         } else if (strncmp(arg[0], "start", 5) ==0 && checkS(DB_LOADED)) {
             pid = fork();
             if (pid) {
                 dbstate = DB_RUNNING;
                 wait(NULL);
+                printf("%s started as pid %d\n", elf_name, pid);
             } else {
                 ptrace(PTRACE_TRACEME,0,NULL,NULL);
                 execve(elf_name,argv, environ);
             }
         } else if (strncmp(arg[0], "vmmap", 5) ==0 || arg[0][0] == 'm') {
             if (dbstate == DB_LOADED) {
-
+                // TODO
             } else if (dbstate == DB_RUNNING) {
                 char file_name[100];
                 sprintf(file_name, "/proc/%d/maps", pid);
@@ -135,18 +125,54 @@ int main(int argc, char**argv) {
                     printf("Open file %s failed\n", file_name);
                     continue;
                 }
-                fgets(buf, N,f);
                 while(fgets(buf, N, f) != NULL) {
                     printf("%s", buf);
                 }
                 fclose(f);
             }
+
+        } else if ((strncmp(arg[0], "break", 5) ==0 || arg[0][0] == 'b') && checkS(DB_RUNNING | DB_LOADED)) {
+            if (arg_count < 2) {
+                puts("No argrument");
+                continue;
+            }
+            void *addr;
+            int ret = sscanf(arg[1], "0x%p",&addr);
+            if (ret < 1) {
+                puts("Addr should be 0x_____");
+                continue;
+            }
+            for (int i = 0; i < bpcount+1; i++) {
+                if (bps[i].used == 0) {
+                    bps[i].used = 1;
+                    bps[i].addr = addr;
+                    bpcount++;
+                    break;
+                }
+            }
+
+        } else if (strncmp(arg[0], "list", 4) ==0 || arg[0][0] == 'l') {
+            if (bpcount==0) {
+                puts("No break point set");
+            } else {
+                int cur = 0;
+                for (int i = 0; i < bpcount; i++,cur++) {
+                    if (bps[cur].used != 1) {
+                        cur++;
+                    }
+                    printf("Break point #%d: 0x%p\n", cur, bps[cur].addr);
+                }
+            }
+        } else if (strncmp(arg[0], "help", 4) ==0 || arg[0][0] == 'h') {
+			print_help();
+
         } else if (strncmp(arg[0], "si", 2)==0 && checkS(DB_RUNNING)) {
             if (ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL) < 0) {
                 puts("error");
                 break;
             }
             wait(NULL);
+
         } else if (strncmp(arg[0], "exit", 4)==0) {
 EXIT:
             if (dbstate == DB_RUNNING) {
@@ -203,4 +229,59 @@ void print_eip(pid_t pid) {
     struct user_regs_struct regs;
     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
     printf(" @0x%llx", regs.rip);
+}
+
+void print_elf(char *elf, elf_handle_t *eh) {
+    elf_strtab_t *tab = NULL;
+    if (!eh) {
+        puts("Failed");
+        return;
+    }
+    elf_load_all(eh);
+    for (tab = eh->strtab; tab != NULL; tab = tab->next) {
+        if (tab->id == eh->shstrndx) break;
+    }
+    int i;
+    for (i = 0; i < eh->shnum; i++) {
+        if (strncmp(&tab->data[eh->shdr[i].name], ".text", 5) == 0) {
+            break;
+        }
+    }
+    elf_shdr_t text_section = eh->shdr[i];
+    printf("program '%s' loaded. Entry point 0x%lx. [TEXT] vaddr:0x%llx, offset:0x%llx, size:0x%llx\n", elf, eh->entrypoint, text_section.addr,  text_section.offset, text_section.size);
+    elf_close(eh); 
+}
+
+void print_help() {
+char *help_meg = "\
+[States]\n\
+{Initial, Loaded, Running}\n\
+\n\
+[Commands]\n\
+<Inital>\n\
+    load {path/to/a/program}: load a elf\n\
+\n\
+<Loaded>\n\
+    start: start the program and stop at the first instruction\n\
+\n\
+<Running>\n\
+    dump (x) addr [length]: dump memory content\n\
+    cont (c): continue execution\n\
+    getregs: show registers\n\
+    set (s) reg val: get a single value to a register\n\
+    si: step into instruction\n\
+\n\
+<Loaded or Running>\n\
+    disasm (d) addr: disassemble instructions in a file or a memory region\n\
+    break (b) {instruction-address}: add a break point\n\
+    run (r) : run the program\n\
+    vmmap (m) : show memory layout\n\
+\n\
+<Any>\n\
+    help (h) : show this message\n\
+    list (l) : list break points\n\
+    delete {break-point-id}: remove a break point\n\
+    exit (q) : terminate the debugger\n\
+";
+	puts(help_meg);
 }
